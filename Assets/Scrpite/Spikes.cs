c94ea7051a0c8b39ccd624a02a29df4da6a84bbd
Being <139492230+Beinngg@users.noreply.github.com>
2025-06-14 20:16:29 +0800

    spikes complete

--- Assets/Scrpite/Spikes.cs
+++ Assets/Scrpite/Spikes.cs
@@ -2,20 +2,23 @@ using UnityEngine;
 
 public class Spikes : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     float damage = 40f;
+
     public void TakeDamage(PlayerBehaviour player)
     {
-        player.ModifyHealth(-damage);
+        player.ModifyHealth(-damage * Time.deltaTime); // Damage over time
         // Optionally, add sound or animation here
     }
+
     public void OnCollisionStay(Collision other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
             PlayerBehaviour player = other.gameObject.GetComponent<PlayerBehaviour>();
-
+            if (player != null)
+            {
+                TakeDamage(player);
+            }
         }
     }
-
 }
